e3d2c48654f345ba22421c00fcbfa4296f3a8612
Oliwia Kuczyńska <ko49317>
2023-10-30 14:39:00 +0100

    lab d

--- src/Entity/Location.php
+++ src/Entity/Location.php
@@ -36,6 +36,10 @@ class Location
         $this->measurements = new ArrayCollection();
     }
 
+    public function __toString() {
+        return (string) $this->city;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
---
+++ src/Form/MeasurementType.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Measurement;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MeasurementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        // $builder
+        //     ->add('date', DateType::class, [
+        //         'widget' => 'single_text',
+        //         'html5' => true,
+        //     ])
+        //     ->add('celsius', NumberType::class, [
+        //         'attr' => [
+        //             'min' => -20,
+        //             'max' => 40,
+        //         ],
+        //         'html5' => true,
+        //     ])
+        //     ->add('location', EntityType::class, [
+        //         'class' => 'App\Entity\Location',
+        //         'choice_label' => 'city',
+        //     ])
+        //     ->add('wind', NumberType::class, [
+        //         'attr' => [
+        //             'min' => 0,
+        //             'max' => 200,
+        //         ],
+        //         'html5' => true,
+        //     ])
+        //     ->add('precipitation', NumberType::class, [
+        //         'attr' => [
+        //             'min' => 0,
+        //             'max' => 100,
+        //         ],
+        //         'html5' => true,
+        //     ])
+        // ;
+
+        $builder
+        ->add('date', DateType::class, [
+                'widget' => 'single_text',
+                     'html5' => true,
+                 ])
+        ->add('celsius')
+        ->add('location')
+        ->add('wind')
+        ->add('precipitation')
+        ;
+    }
+
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Measurement::class,
+        ]);
+    }
+}
